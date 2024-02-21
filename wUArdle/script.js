a9a3fc432b32715dea13662e8dac33744c369365
Nathan Tebbs <111322376+nathantebbs@users.noreply.github.com>
2024-02-21 14:36:37 -0700

    base game implemented

--- wUArdle/script.js
+++ wUArdle/script.js
@@ -24,7 +24,101 @@ function initBoard(params) {
   }
 }
 
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return
+  }
+  pressedKey = pressedKey.toLowerCase()
 
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+  let box = row.children[nextLetter]
+  box.textContent = pressedKey
+  console.log("pressedKey")
+  box.classList.add("filled-box")
+  currentGuess.push(pressedKey)
+  nextLetter += 1
+}
+
+function deleteLetter() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+  let box = row.children[nextLetter - 1]
+  box.textContent = ""
+  box.classList.remove("filled-box")
+  currentGuess.pop()
+  nextLetter -= 1
+}
+
+function checkGuess() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+  let guessString = ""
+  let rightGuess = Array.from(rightGuessString)
+  for (const val of currentGuess) {
+    guessString += val
+  }
+  if (guessString.length != 5) {
+    alert("Not enogh letters!")
+    return
+  }
+
+  if (!WORDS.includes(guessString)) {
+    alert("Word not found in list!")
+    return
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let letterColor = ""
+    let box = row.children[i]
+    let letter = currentGuess[i]
+    
+    let letterPosition = rightGuess.indexOf(currentGuess[i])
+    if (letterPosition === -1) {
+      letterColor = 'grey'
+    } else {
+      if (currentGuess[i] === rightGuess[i]) {
+        letterColor = 'green'
+      } else {
+        letterColor = 'yellow'
+      }
+      rightGuess[letterPosition] = "#"
+    }
+
+    let delay = 250 * i;
+    setTimeout(() => {
+      box.style.backgroundColor = letterColor
+      shadeKeyboard(letter, letterColor)
+    }, delay);
+  }
+
+  if (guessString === rightGuessString) {
+    alert("You guessed right! Game over!")
+    guessesRemaining = 0
+    return
+  } else {
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (guessesRemaining === 0) {
+      alert("You don't have any guesses remaining. You lost!")
+    }
+  }
+}
+
+function shadeKeyboard(letter, color) {
+  for (const elem of document.getElementsByClassName("keyboard-button")) {
+    let oldColor = elem.style.backgroundColor
+    if (oldColor === "green") {
+      return
+    }
+
+    if (oldColor === "yellow" && color !== "green") {
+      return
+    }
+
+    elem.style.backgroundColor = color
+    break
+  }
+}
 
 document.addEventListener("keyup", (e) => {
   if (guessesRemaining === 0) {
@@ -36,13 +130,16 @@ document.addEventListener("keyup", (e) => {
     return
   }
 
-  let found = pressedKey.match(/[a-z]/gi)
   if (pressedKey === "Enter") {
-    if (!found || found.length > 1) {
-      return
-    } else {
-      insertLetter(pressedKey)
-    }
+    checkGuess()
+    return
+  }
+
+  let found = pressedKey.match(/[a-z]/gi)
+  if (!found || found.length > 1) {
+    return
+  } else {
+    insertLetter(pressedKey)
   }
 })
 
